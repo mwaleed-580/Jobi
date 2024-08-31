58c3df672197f0c54913730c00071d147371114e
Muhammad Waleed <waleedurahman@gmail.com>
2024-09-01 01:42:59 +0500

    Categories added in header

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$database = "jobi";
+
+$conn = mysqli_connect($hostname, $username, $password, $database);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
