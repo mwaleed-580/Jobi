99d1062359b14b6d7088dce41902a9affe1b35b8
Muhammad Waleed <waleedurahman@gmail.com>
2024-09-01 01:15:43 +0500

    Header Footer Created

---
+++ includes/footer.php
@@ -0,0 +1,132 @@
+<!--=====================================================Footer=====================================================-->
+<div class="footer-one">
+    <div class="container">
+        <div class="inner-wrapper">
+            <div class="row">
+                <div class="col-lg-2 col-md-3 footer-intro mb-15">
+                    <div class="logo mb-15">
+                        <a href="index.html" class="d-flex align-items-center">
+                            <img src="assets/images/logo/logo_03.png" alt="" />
+                        </a>
+                    </div>
+                    <img
+                        src="assets/images/lazy.svg"
+                        data-src="https://html.creativegigstf.com/jobi/jobi/images/shape/shape_28.svg"
+                        alt=""
+                        class="lazy-img mt-80 sm-mt-30 sm-mb-20" />
+                    <!-- logo -->
+                </div>
+                <div class="col-lg-2 col-md-3 col-sm-4 mb-20">
+                    <h5 class="footer-title">Services​</h5>
+                    <ul class="footer-nav-link style-none">
+                        <li><a href="job-grid-v2.html">Browse Jobs</a></li>
+                        <li><a href="company-v1.html">Companies</a></li>
+                        <li><a href="candidates-v1.html">Candidates</a></li>
+                        <li><a href="pricing.html">Pricing</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-2 col-md-3 col-sm-4 mb-20">
+                    <h5 class="footer-title">Company</h5>
+                    <ul class="footer-nav-link style-none">
+                        <li><a href="about-us.html">About us</a></li>
+                        <li><a href="blog-v2.html">Blogs</a></li>
+                        <li><a href="faq.html">FAQ’s</a></li>
+                        <li><a href="contact.html">Contact</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-2 col-md-3 col-sm-4 mb-20">
+                    <h5 class="footer-title">Support</h5>
+                    <ul class="footer-nav-link style-none">
+                        <li><a href="contact.html">Terms of use</a></li>
+                        <li><a href="contact.html">Terms & conditions</a></li>
+                        <li><a href="contact.html">Privacy</a></li>
+                        <li><a href="contact.html">Cookie policy</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-4 mb-20 footer-newsletter">
+                    <h5 class="footer-title">Newsletter</h5>
+                    <p>Join & get important new regularly</p>
+                    <form action="#" class="d-flex">
+                        <input type="email" placeholder="Enter your email*" />
+                        <button>Send</button>
+                    </form>
+                    <p class="note">
+                        We only send interesting and relevant emails.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <!-- /.inner-wrapper -->
+    </div>
+    <div class="bottom-footer">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-4 order-lg-3 mb-15">
+                    <ul
+                        class="style-none d-flex order-lg-last justify-content-center justify-content-lg-end social-icon">
+                        <li>
+                            <a href="#"><i class="bi bi-whatsapp"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="bi bi-dribbble"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="bi bi-google"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="bi bi-instagram"></i></a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-lg-4 order-lg-1 mb-15">
+                    <ul
+                        class="d-flex style-none bottom-nav justify-content-center justify-content-lg-start">
+                        <li><a href="contact.html">Privacy & Terms.</a></li>
+                        <li><a href="contact.html"> Contact Us</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-4 order-lg-2">
+                    <p class="text-center mb-15">Copyright @2023 jobi inc.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- /.footer -->
+
+
+<button class="scroll-top">
+    <i class="bi bi-arrow-up-short"></i>
+</button>
+
+<!-- Optional JavaScript _____________________________  -->
+
+<!-- jQuery first, then Bootstrap JS -->
+<!-- jQuery -->
+<script src="assets/vendor/jquery.min.js"></script>
+<!-- Bootstrap JS -->
+<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- WOW js -->
+<script src="assets/vendor/wow/wow.min.js"></script>
+<!-- Slick Slider -->
+<script src="assets/vendor/slick/slick.min.js"></script>
+<!-- Fancybox -->
+<script src="assets/vendor/fancybox/dist/jquery.fancybox.min.js"></script>
+<!-- Lazy -->
+<script src="assets/vendor/jquery.lazy.min.js"></script>
+<!-- js Counter -->
+<script src="assets/vendor/jquery.counterup.min.js"></script>
+<script src="assets/vendor/jquery.waypoints.min.js"></script>
+<!-- Nice Select -->
+<script src="assets/vendor/nice-select/jquery.nice-select.min.js"></script>
+<!-- validator js -->
+<script src="assets/vendor/validator.js"></script>
+
+<!-- Theme js -->
+<script src="assets/js/theme.js"></script>
+</div>
+<!-- /.main-page-wrapper -->
+</body>
+
+</html>
---
+++ includes/header.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta
+        name="keywords"
+        content="Digital marketing agency, Digital marketing company, Digital marketing services" />
+    <meta
+        name="description"
+        content="Jobi is a beautiful website template designed for job board websites." />
+    <meta property="og:site_name" content="Jano" />
+    <meta property="og:url" content="https://creativegigstf.com/" />
+    <meta property="og:type" content="website" />
+    <meta
+        property="og:title"
+        content="Jobi - Responsive Job Board HTML Template" />
+    <meta name="og:image" content="assets/images/assets/ogg.png" />
+    <!-- For IE -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- For Resposive Device -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- For Window Tab Color -->
+    <!-- Chrome, Firefox OS and Opera -->
+    <meta name="theme-color" content="#244034" />
+    <!-- Windows Phone -->
+    <meta name="msapplication-navbutton-color" content="#244034" />
+    <!-- iOS Safari -->
+    <meta name="apple-mobile-web-app-status-bar-style" content="#244034" />
+    <title>Jobi - Responsive Job Board HTML Template</title>
+    <!-- Favicon -->
+    <link
+        rel="icon"
+        type="image/png"
+        sizes="56x56"
+        href="assets/images/fav-icon/icon.png" />
+    <!-- Bootstrap CSS -->
+    <link
+        rel="stylesheet"
+        type="text/css"
+        href="assets/css/bootstrap.min.css"
+        media="all" />
+    <!-- Main style sheet -->
+    <link
+        rel="stylesheet"
+        type="text/css"
+        href="assets/css/style.min.css"
+        media="all" />
+    <!-- responsive style sheet -->
+    <link
+        rel="stylesheet"
+        type="text/css"
+        href="assets/css/responsive.css"
+        media="all" />
+
+</head>
+
+<body>
